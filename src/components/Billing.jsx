9d63c7dc3ca77baa13efc11fbd630bd108d9a5cb
Robby Novianto <heyitsrobby99@gmail.com>
2023-01-22 12:42:34 +0700

    finish

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -5,13 +5,13 @@ import styles, { layout } from '../style';
 const Billing = () => (
   <section id='product' className={`${layout.sectionReverse}`}>
     <div className={`${layout.sectionImgReverse}`}>
-      <img  data-aos='fade-right'  src={bill} alt="billing" className=' w-[100%] h-[100%] relative z-[5]' />
+      <img  data-aos='fade-right' data-aos-duration='700' src={bill} alt="billing" className=' w-[100%] h-[100%] relative z-[5]' />
 
       <div className='absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient' />
       <div className='absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient' />
 
       </div>
-      <div data-aos='fade-up' className={`${layout.sectionInfo}`}>
+      <div data-aos='fade-up'  data-aos-duration='700' className={`${layout.sectionInfo}`}>
         <h2 className={`${styles.heading2}`}>Easily control your <br className='sm:block hidden' /> billing & invoicing.</h2>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}> Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat.</p>
         <div className='flex flex-row flex-wrap sm:mt-10 mt-6'>
--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -4,14 +4,14 @@ import Button from './Button';
 
 const CardDeal = () => (
   <section className={`${layout.section}`}>
-    <div className={`${layout.sectionInfo}`}>
+    <div data-aos='fade-up' data-aos-duration='700' className={`${layout.sectionInfo}`}>
       <h2 className={`${styles.heading2}`}>Find a better card deal <br className='sm:block hidden'/>in few easy steps.</h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.</p>
       
       <Button styles='mt-10' />
     </div>
 
-    <div className={`${layout.sectionImg}`}>
+    <div data-aos='fade-left' data-aos-duration='700' className={`${layout.sectionImg}`}>
       <img src={card} alt="card" />
 
     </div>
